31c6061e6a5757e4d73f205870ea6ba0e9ac1057
sahil16-12 <sahil16december@gmail.com>
2024-09-18 21:06:02 +0530

    Updated currency symbol

--- script.js
+++ script.js
@@ -69,9 +69,9 @@ function updateValues(){
         amounts.filter(item => item < 0).reduce((acc,item) => (acc += item), 0)* -1
     ).toFixed(2);
 
-    balance.innerText=`$${total}`;
-    money_plus.innerText=`$${income}`;
-    money_minus.innerText=`$${expense}`;
+    balance.innerText=`₹${total}`;
+    money_plus.innerText=`₹${income}`;
+    money_minus.innerText=`₹${expense}`;
 }
 
 
